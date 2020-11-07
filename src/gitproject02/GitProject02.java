65c0b5006460d4e086a17614c0cfa1d46e069d51
John Q <ikram1981@gmail.com>
2020-11-07 11:43:36 -0600

    t

--- src/gitproject02/GitProject02.java
+++ src/gitproject02/GitProject02.java
@@ -7,6 +7,8 @@ public class GitProject02 {
 		System.out.println("Hello World from git-local machine");
 		System.out.println("Hello World-from gitHub");
 		System.out.println("One more from git local");
+		System.out.println("Second more from git local");
+		System.out.println("Second more from git local");
 
 	}
 
