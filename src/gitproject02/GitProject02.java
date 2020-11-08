f573cbe312079ab1877fb65036b9ff3d0fd25359
JohnQ1981 <69947888+JohnQ1981@users.noreply.github.com>
2020-11-08 09:30:49 -0600

    Update GitProject02.java

--- src/gitproject02/GitProject02.java
+++ src/gitproject02/GitProject02.java
@@ -5,6 +5,7 @@ public class GitProject02 {
 	public static void main(String[] args) {
 
 		System.out.println("Hello World from git-local machine");
+		System.out.println("GitHub typed on master");
 
 
 	}
